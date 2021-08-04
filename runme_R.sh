570c32d0e76395b5d16f450dd61617eb0513e3b2
Christophe Roeder <119077+croeder@users.noreply.github.com>
2021-08-04 11:56:57 -0600

    add some feedback to the runme_R.sh script when it fails. Silence is often interpretted in Unix culture as success.

--- runme_R.sh
+++ runme_R.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+FAILURE=0
 
 mkdir -p output
 echo "language | r" > ./output/evaluation.txt
@@ -10,7 +12,35 @@ else
   echo "Version | $1" >> ./output/evaluation.txt
 fi
 
-R CMD BATCH --vanilla '--args mortality' training.R output/training_mortality.Rout
-R CMD BATCH --vanilla '--args fss'       training.R output/training_fss.Rout
-R CMD BATCH --vanilla "--args $1" testing.R output/testing.Rout
-R CMD BATCH --vanilla "--args $1" evaluate.R output/evaluate.Rout
+
+if ! R CMD BATCH --vanilla '--args mortality' training.R output/training_mortality.Rout
+then
+    echo "Training mortality failed. See output/training_mortality.Rout"
+    FAILURE=1
+fi
+
+if ! R CMD BATCH --vanilla '--args fss'       training.R output/training_fss.Rout
+then
+    echo "Training fss failed. See output/training_fss.Rout"
+    FAILURE=1
+fi
+
+if ! R CMD BATCH --vanilla "--args $1" testing.R output/testing.Rout
+then
+    echo "Testing failed. See output/testing.Rout"
+    FAILURE=1
+fi
+
+if ! R CMD BATCH --vanilla "--args $1" evaluate.R output/evaluate.Rout
+then
+    echo "Evaluation failed. See output/evaluate.Rout"
+    FAILURE=1
+fi
+
+if [[ $FAILURE == 0 ]]
+then
+    echo "Success!"
+    echo ""
+    grep -v "seconds elapsed" output/evaluation.txt
+fi
+
